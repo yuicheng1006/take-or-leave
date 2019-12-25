0ab100c22066bb9748811a40e642832d3af13d6d
yuicheng1006 <yuice1006@gmail.com>
2019-12-25 13:37:36 +0800

    重新修改路徑

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,13 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+
+  APIPATH: '"https://take-or-leave.herokuapp.com"',
+  apiKey: '"AIzaSyCpE41f-O3QBpiqJ_CfuPdcY83vWlFXhg0"',
+  authDomain: '"takeorleave-45e27.firebaseapp.com"',
+  databaseURL: '"https://takeorleave-45e27.firebaseio.com"',
+  projectId: '"takeorleave-45e27"',
+  storageBucket: '"takeorleave-45e27.appspot.com"',
+  messagingSenderId: '"365089696019"',
+  appId: '"1:365089696019:web:65a9a57498cfd52fe292e6"',
 }
